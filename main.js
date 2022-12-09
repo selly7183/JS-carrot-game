959df6bf40944b2338ff3374543ed5ca1ce1fa70
selly7183 <selly7183@naver.com>
2022-12-09 11:39:37 +0900

    game timer

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 const CARROT_SIZE = 80;
 const CARROT_COUNT = 5;
 const BUG_COUNT = 5;
+const GAME_DURATION_SEC = 5;
 
 const field = document.querySelector(".game__field");
 const fieldRect = field.getBoundingClientRect();
@@ -43,7 +44,23 @@ function showTimerAndScore() {
 	gameScore.style.visibility = "visible";
 }
 
-function startGameTimer() {}
+function startGameTimer() {
+	let remainingTimeSec = GAME_DURATION_SEC;
+	updateTimerText(remainingTimeSec);
+	timer = setInterval(() => {
+		if (remainingTimeSec <= 0) {
+			clearInterval(timer);
+			return;
+		}
+		updateTimerText(--remainingTimeSec);
+	}, 1000);
+}
+
+function updateTimerText(time) {
+	const minutes = Math.floor(time / 60);
+	const seconds = time % 60;
+	gameTimer.innerText = `${minutes}:${seconds}`;
+}
 
 function initGame() {
 	field.innerHTML = "";
