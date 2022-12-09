19013ba4d2da2bf10a97686dc1feafccac531f6e
selly7183 <selly7183@naver.com>
2022-12-09 13:30:45 +0900

    sound 추가

--- main.js
+++ main.js
@@ -15,6 +15,12 @@ const popUp = document.querySelector(".pop-up");
 const popUpText = document.querySelector(".pop-up__message");
 const popUpRefresh = document.querySelector(".pop-up__refresh");
 
+const carrotSound = new Audio("./sound/carrot_pull.mp3");
+const alertSound = new Audio("./sound/alert.wav");
+const bgSound = new Audio("./sound/bg.mp3");
+const bugSound = new Audio("./sound/bug_pull.mp3");
+const winSound = new Audio("./sound/game_win.mp3");
+
 let started = false;
 let score = 0;
 let timer = undefined;
@@ -40,18 +46,28 @@ function startGame() {
 	showStopButton();
 	showTimerAndScore();
 	startGameTimer();
+	playSound(bgSound);
 }
 
 function stopGame() {
 	started = false;
 	stopGameTimer();
 	hideGameButton();
-	showPopUpWidthText("REPLAY?");
+	showPopUpWidthText("REPLAY❓");
+	playSound(alertSound);
+	stopSound(bgSound);
 }
 
 function finishGame(win) {
 	started = false;
 	hideGameButton();
+	if (win) {
+		playSound(winSound);
+	} else {
+		playSound(bugSound);
+	}
+	stopGameTimer();
+	stopSound(bgSound);
 	showPopUpWidthText(win ? "YOU WON!" : "YOU LOST!");
 }
 
@@ -59,6 +75,7 @@ function showStopButton() {
 	const icon = gameBtn.querySelector(".fas");
 	icon.classList.add("fa-stop");
 	icon.classList.remove("fa-play");
+	gameBtn.style.visibility = "visible";
 }
 
 function hideGameButton() {
@@ -103,6 +120,7 @@ function hidePopUp() {
 }
 
 function initGame() {
+	score = 0;
 	field.innerHTML = "";
 	gameScore.innerText = CARROT_COUNT;
 	// 벌레와 당근을 생성한 뒤 field에 추가해줌.
@@ -118,14 +136,23 @@ function onFiledClick(event) {
 	if (target.matches(".carrot")) {
 		target.remove();
 		score++;
+		playSound(carrotSound);
 		updateScoreBoard();
 		if (score === CARROT_COUNT) {
 			finishGame(true);
 		}
 	} else if (target.matches(".bug")) {
-		stopGameTimer();
 		finishGame(false);
 	}
+}
+
+function playSound(sound) {
+	sound.currentTime = 0;
+	sound.play();
+}
+
+function stopSound(sound) {
+	sound.pause();
 }
 
 function updateScoreBoard() {
