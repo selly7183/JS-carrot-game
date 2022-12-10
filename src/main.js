32f0d43dc25e3242589ef7169f968f946ed19b75
selly7183 <selly7183@naver.com>
2022-12-10 16:43:11 +0900

    timer, carrot, bug 갯수 수정

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 "use strict";
 
 const CARROT_SIZE = 80;
-const CARROT_COUNT = 5;
-const BUG_COUNT = 5;
-const GAME_DURATION_SEC = 5;
+const CARROT_COUNT = 10;
+const BUG_COUNT = 10;
+const GAME_DURATION_SEC = 10;
 
 const field = document.querySelector(".game__field");
 const fieldRect = field.getBoundingClientRect();
@@ -68,7 +68,7 @@ function finishGame(win) {
 	}
 	stopGameTimer();
 	stopSound(bgSound);
-	showPopUpWidthText(win ? "YOU WON!" : "YOU LOST!");
+	showPopUpWidthText(win ? "YOU WON 😆" : "YOU LOST 😭");
 }
 
 function showStopButton() {
