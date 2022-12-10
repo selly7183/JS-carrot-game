605ded6b8c1c914103fc2e271245afa9a84f6467
selly7183 <selly7183@naver.com>
2022-12-11 00:29:42 +0900

    repectoring 마무리

--- src/game.js
+++ src/game.js
@@ -1,5 +1,5 @@
 "use strict";
-import Field from "./field.js";
+import { Field, ItemType } from "./field.js";
 import * as sound from "./sound.js";
 
 export const Reason = Object.freeze({
@@ -43,7 +43,7 @@ class Game {
 
 		this.gameBtn.addEventListener("click", () => {
 			if (this.started) {
-				this.stop();
+				this.stop(Reason.cancel);
 			} else {
 				this.start();
 			}
@@ -70,40 +70,26 @@ class Game {
 		sound.playBackground();
 	}
 
-	stop() {
+	stop(reason) {
 		this.started = false;
 		this.stopGameTimer();
 		this.hideGameButton();
-		sound.playAlert();
 		sound.stopBackground();
-		this.onGameStop && this.onGameStop(Reason.cancel);
-	}
-
-	finish(win) {
-		this.started = false;
-		this.hideGameButton();
-		if (win) {
-			sound.playWin();
-		} else {
-			sound.playBug();
-		}
-		this.stopGameTimer();
-		sound.stopBackground();
-		this.onGameStop && this.onGameStop(win ? Reason.win : Reason.lose);
+		this.onGameStop && this.onGameStop(reason);
 	}
 
 	onItemClick = (item) => {
 		if (!this.started) {
 			return;
 		}
-		if (item === "carrot") {
+		if (item === ItemType.carrot) {
 			this.score++;
 			this.updateScoreBoard();
 			if (this.score === this.carrotCount) {
-				this.finish(true);
+				this.stop(Reason.win);
 			}
-		} else if (item === "bug") {
-			this.finish(false);
+		} else if (item === ItemType.bug) {
+			this.stop(Reason.lose);
 		}
 	};
 
@@ -129,7 +115,7 @@ class Game {
 		this.timer = setInterval(() => {
 			if (remainingTimeSec <= 0) {
 				clearInterval(this.timer);
-				this.finish(this.carrotCount === this.score);
+				this.stop(this.carrotCount === this.score ? Reason.win : Reason.lose);
 				return;
 			}
 			this.updateTimerText(--remainingTimeSec);
